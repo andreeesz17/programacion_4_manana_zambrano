5b518437863e177a310d5fe6c54095a2b04b8feb
andreeesz17 <andryzambrano1709@gmail.com>
2026-04-09 11:06:54 -0500

    Clase 2026-04-09

--- modulo_1/basics/07_operadores_comparacion.kt
+++ modulo_1/basics/07_operadores_comparacion.kt
@@ -1,19 +1,15 @@
-/**
- * You can edit, run, and share this code.
- * play.kotlinlang.org
- */
-fun main() {
-    
-    //readline
-    println("Operadores Comparacion ")
-	val numero1 = 10
-	val numero2 = 2
+// Online Kotlin compiler to run Kotlin program online
+// Print "Try programiz.pro" message
 
-    println("$numero1 == $numero2 : ${numero1==numero2}")
-    println("$numero1 != $numero2 : ${numero1!=numero2}")
-    println("$numero1 > $numero2 : ${numero1>numero2}")
-    println("$numero1 < $numero2 : ${numero1<numero2}")
-    println("$numero1 >= $numero2 : ${numero1>=numero2}")
-    println("$numero1 <= $numero2 : ${numero1<=numero2}")
-    
-}   
+fun main() {
+  println("Operadores de comparación")
+  val numero1 = 15
+  val numero2 = 10
+  println("$numero1==$numero2=${numero1==numero2}")
+  println("$numero1 .equals $numero2=${numero1.equals(numero2)}")
+  println("$numero1!=$numero2=${numero1!=numero2}")
+  println("$numero1>$numero2=${numero1>numero2}")
+  println("$numero1>=$numero2=${numero1>=numero2}")
+  println("$numero1<$numero2=${numero1<numero2}")
+  println("$numero1<=$numero2=${numero1<=numero2}")
+}
---
+++ modulo_1/basics/08_operadores_logicos.kt
@@ -0,0 +1,22 @@
+// Online Kotlin compiler to run Kotlin program online
+// Print "Try programiz.pro" message
+
+fun main() {
+  println("Operadores Logicos")
+  val esMayor=true
+  val tienePermiso=false
+  val estaActivo=true
+ 
+  println("&& - And Logico")
+  println("$esMayor && $tienePermiso=${esMayor&&tienePermiso}")
+  println("$estaActivo && $esMayor=${estaActivo&&esMayor}")
+  
+  println("|| - Or Logico")
+  println("$esMayor || $tienePermiso=${esMayor||tienePermiso}")
+  println("$estaActivo || $esMayor=${estaActivo||esMayor}")
+  
+  println("! - Not Logico")
+  println("! $esMayor=${!esMayor}")
+  println("! $estaActivo=${!estaActivo}")
+
+}
