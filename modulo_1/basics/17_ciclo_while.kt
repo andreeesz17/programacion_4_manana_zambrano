1555f36765a41c01623d87c24e1bfa7fae14467e
andreeesz17 <andryzambrano1709@gmail.com>
2026-04-13 21:59:46 -0500

    Clase 13-04-2026

---
+++ modulo_1/basics/17_ciclo_while.kt
@@ -0,0 +1,9 @@
+fun main() {
+  println("Controles de Flujo Iteraciones, Ciclos repetitivos-Ciclo While")
+  print("While Básico")
+  var contador = 1
+  while(contador <= 5){
+      prinln(contador)
+      contador++;
+  }
+}
