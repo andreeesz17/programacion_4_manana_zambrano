181da96fc7f4c58a6d637f994290c02278ba9bcc
andreeesz17 <andryzambrano1709@gmail.com>
2026-07-06 10:04:17 -0500

    flutter_shop_app checkpoint 02-06-07-2026

--- modulo_3/flutter/flutter_shop_app/test/widget_test.dart
+++ modulo_3/flutter/flutter_shop_app/test/widget_test.dart
@@ -1,24 +1,11 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// test/widget_test.dart
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:flutter_shop_app/main.dart';
-import 'package:flutter_shop_app/core/config/app_config.dart';
 
 void main() {
-  testWidgets('Verification screen smoke test', (WidgetTester tester) async {
-    // Load dotenv mock data for the test environment.
-    dotenv.testLoad(fileInput: 'API_BASE_URL=https://higuera-shopapi.uaeftt-ute.site/api');
-
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const FlutterShopApp());
-
-    // Verify that our app name is displayed.
-    expect(find.text(AppConfig.appName), findsOneWidget);
+  test('FlutterShopApp can be constructed', () {
+    expect(const FlutterShopApp(), isA<FlutterShopApp>());
   });
 }
